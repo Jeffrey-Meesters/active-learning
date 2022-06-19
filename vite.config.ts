a0dc2c01762623f99aa61427de80c58d9ec47772
Jeffrey Meesters <jeffrey.meesters@nl.abnamro.com>
2022-06-19 15:31:23 +0200

    Setup eslint with airbnb

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  envPrefix: 'CLIENT_' // Never set this as an empty string: https://vitejs.dev/config/index.html#envprefix
-})
+  envPrefix: 'CLIENT_', // Never set this as an empty string: https://vitejs.dev/config/index.html#envprefix
+  server: {
+    port: 8082,
+  },
+});
